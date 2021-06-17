68e1634a2a88fe6cd9b4d4cf58efe9237ec69566
david <calhoun5321@gmail.com>
2021-06-16 22:07:42 -0700

    adds theme to index.html, app.vue, and index.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home |</router-link>
+    <!-- Header -->
+    <header id="header">
+      <a class="logo">
+        <strong>Kind of Vegan</strong>
+      </a>
+      <router-link to="/">Home</router-link>
+
+      <router-link v-if="!isLoggedIn()" to="/signup">Signup</router-link>
+
+      <router-link v-if="!isLoggedIn()" to="/login">Login</router-link>
+
+      <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link>
+
+      <router-link v-if="isLoggedIn()" to="/recipes">Recipes Index</router-link>
+
+      <router-link v-if="isLoggedIn()" to="/recipe/new">New Recipe</router-link>
+    </header>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link>
 
       <router-link v-if="!isLoggedIn()" to="/signup">Signup |</router-link>
 
@@ -12,33 +29,20 @@
       <router-link v-if="isLoggedIn()" to="/recipes">Recipes Index |</router-link>
 
       <router-link v-if="isLoggedIn()" to="/recipe/new">New Recipe</router-link>
-    </div>
+    </div> -->
     <router-view />
+
+    <footer id="footer">
+      <p class="copyright">
+        &copy; Untitled. All rights reserved. Design:
+        <a href="https://html5up.net">HTML5 UP</a>
+        .
+      </p>
+    </footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
--- src/views/Recipes/Index.vue
+++ src/views/Recipes/Index.vue
@@ -4,15 +4,29 @@
     <h3>Search Recipes by Name or Ingredients:</h3>
     <input ype="text" v-model="filter" placeholder="ie garlic, pasta, tomato" />
     <div v-for="recipe in filterBy(recipes, filter, 'name', 'ingredients')" v-bind:key="recipe.id">
-      <router-link :to="`/recipes/${recipe.id}`">
+      <!-- <router-link :to="`/recipes/${recipe.id}`">
         <h4>Title: {{ recipe.name }}</h4>
-      </router-link>
-      <h4>Chef: {{ recipe.chef }}</h4>
+      </router-link> -->
+      <!-- <h4>Chef: {{ recipe.chef }}</h4>
       <h4>Ingredients: {{ recipe.ingredients }}</h4>
       <h4>Directions: {{ recipe.directions }}</h4>
-      <h4>Best Eaten: {{ recipe.best_eaten }}</h4>
+      <h4>Best Eaten: {{ recipe.best_eaten }}</h4> -->
 
-      <h1>__</h1>
+      <div class="posts">
+        <article>
+          <a href="#" class="image"><img :src="recipe.image" alt="" /></a>
+          <router-link :to="`/recipes/${recipe.id}`">
+            <h4>Title: {{ recipe.name }}</h4>
+          </router-link>
+          <h4>Chef: {{ recipe.chef }}</h4>
+          <h4>Ingredients: {{ recipe.ingredients }}</h4>
+          <h4>Directions: {{ recipe.directions }}</h4>
+          <h4>Best Eaten: {{ recipe.best_eaten }}</h4>
+          <ul class="actions">
+            <li><router-link :to="`/recipes/${recipe.id}`" class="button">view</router-link></li>
+          </ul>
+        </article>
+      </div>
     </div>
   </div>
 </template>
