736cb99c4f6c7ae01610d6c0bc5e1785f3f4aa2f
david <calhoun5321@gmail.com>
2021-06-22 20:24:46 -0700

    adds bootstrap nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home |</router-link>
-
-      <router-link v-if="!isLoggedIn()" to="/signup">Signup |</router-link>
-
-      <router-link v-if="!isLoggedIn()" to="/login">Login |</router-link>
-
-      <router-link v-if="isLoggedIn()" to="/logout">Logout |</router-link>
-
-      <router-link v-if="isLoggedIn()" to="/recipes">Recipes Index |</router-link>
-
-      <router-link v-if="isLoggedIn()" to="/recipe/new">New Recipe</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Kind of Vegan</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link active">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link v-if="!isLoggedIn()" to="/signup" class="nav-link active">Signup</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link v-if="!isLoggedIn()" to="/login" class="nav-link active">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link v-if="isLoggedIn()" to="/logout" class="nav-link active">Logout</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link v-if="isLoggedIn()" to="/recipes" class="nav-link active">Recipes Index</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link v-if="isLoggedIn()" to="/recipe/new" class="nav-link active">New Recipe</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
